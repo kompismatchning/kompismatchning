bf28602adfec3db8a872a507a3d24a9c68f92f57
Jens Ljungblad <jens.ljungblad@gmail.com>
2016-05-03 22:18:24 +0200

    Show matching created at

--- admin/app/services/admin/matching_service.rb
+++ admin/app/services/admin/matching_service.rb
@@ -2,7 +2,7 @@ module Admin
   class MatchingService
     include Godmin::Resources::ResourceService
 
-    attrs_for_index :established, :newcomer
+    attrs_for_index :established, :newcomer, :created_at
     attrs_for_show :established, :newcomer
   end
 end
