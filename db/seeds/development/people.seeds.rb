2c0a60858c1a343d7817b753fbd2ed266cb3b8b8
Jens Ljungblad <jens.ljungblad@gmail.com>
2016-05-09 21:59:08 +0200

    Add seedbank so we can seed in production

---
+++ db/seeds/development/people.seeds.rb
@@ -0,0 +1,44 @@
+Person.create!(
+  name: "Johanna",
+  gender: :female,
+  age: 27,
+  email: "johanna@example.com",
+  established: true,
+  engaged: false
+)
+
+Person.create!(
+  name: "Jens",
+  gender: :male,
+  age: 29,
+  email: "jens@example.com",
+  established: false,
+  engaged: true
+)
+
+Person.create!(
+  name: "Fanny",
+  gender: :female,
+  age: 29,
+  email: "fanny@example.com",
+  established: true,
+  engaged: true
+)
+
+Person.create!(
+  name: "Bruno",
+  gender: :male,
+  age: 29,
+  email: "bruno@example.com",
+  established: false,
+  engaged: true
+)
+
+Person.create!(
+  name: "Julia",
+  gender: :female,
+  age: 29,
+  email: "julia@example.com",
+  established: true,
+  engaged: false
+)
