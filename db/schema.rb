b728405f0132651c5036cca3b0b169b42b2dc686
Johanna Torbjörnsson <jt@johannatorbjornsson.se>
2016-06-28 19:33:14 +0200

    Add fix db schema gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,53 +12,52 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160615181216) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admin_admin_users", force: :cascade do |t|
-    t.string   "email"
-    t.text     "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.text "password_digest"
+    t.datetime "updated_at", null: false
   end
 
   create_table "interest_taggings", force: :cascade do |t|
-    t.integer  "person_id"
-    t.integer  "interest_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.integer "interest_id"
+    t.integer "person_id"
+    t.datetime "updated_at", null: false
   end
 
   add_index "interest_taggings", ["interest_id"], name: "index_interest_taggings_on_interest_id", using: :btree
   add_index "interest_taggings", ["person_id"], name: "index_interest_taggings_on_person_id", using: :btree
 
   create_table "interests", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
   create_table "matches", force: :cascade do |t|
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "newcomer_id"
-    t.integer  "established_id"
-    t.text     "comment"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.integer "established_id"
+    t.integer "newcomer_id"
+    t.datetime "updated_at", null: false
   end
 
   create_table "people", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "gender"
-    t.integer  "age"
-    t.string   "email"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.boolean  "engaged",      default: false
-    t.integer  "status"
-    t.text     "comment"
-    t.string   "phone_number"
-    t.string   "country"
+    t.integer "age"
+    t.text "comment"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.boolean "engaged", default: false
+    t.integer "gender"
+    t.string "name"
+    t.string "phone_number"
+    t.integer "status"
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "interest_taggings", "interests"
