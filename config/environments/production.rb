dadf6af4025a36df9d649e2eacfda3111f9c824a
Jens Ljungblad <jens.ljungblad@gmail.com>
2016-08-15 22:39:45 +0200

    Use sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,13 +84,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # ActionMailer::Base.smtp_settings = {
+  #   port: ENV["MAILGUN_SMTP_PORT"],
+  #   address: ENV["MAILGUN_SMTP_SERVER"],
+  #   user_name: ENV["MAILGUN_SMTP_LOGIN"],
+  #   password: ENV["MAILGUN_SMTP_PASSWORD"],
+  #   domain: "blending-staging.herokuapp.com",
+  #   authentication: :plain
+  # }
   ActionMailer::Base.smtp_settings = {
-    port: ENV["MAILGUN_SMTP_PORT"],
-    address: ENV["MAILGUN_SMTP_SERVER"],
-    user_name: ENV["MAILGUN_SMTP_LOGIN"],
-    password: ENV["MAILGUN_SMTP_PASSWORD"],
-    domain: "blending-staging.herokuapp.com",
-    authentication: :plain
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
   ActionMailer::Base.delivery_method = :smtp
 end
