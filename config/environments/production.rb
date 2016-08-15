bc73395eaf59ef5f9fc2fb82119084fbe98aab19
Jens Ljungblad <jens.ljungblad@gmail.com>
2016-08-15 22:33:15 +0200

    Set mailgun domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     address: ENV["MAILGUN_SMTP_SERVER"],
     user_name: ENV["MAILGUN_SMTP_LOGIN"],
     password: ENV["MAILGUN_SMTP_PASSWORD"],
-    # domain: "yourapp.heroku.com",
+    domain: "blending-staging.herokuapp.com",
     authentication: :plain
   }
   ActionMailer::Base.delivery_method = :smtp
