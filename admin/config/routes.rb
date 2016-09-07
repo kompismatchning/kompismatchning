c63b504b5dab846b1a2f6a7aaa6d2e708d123e3a
Jens Ljungblad <jens.ljungblad@gmail.com>
2016-09-07 21:57:39 +0200

    Add engage action to people list

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -3,6 +3,8 @@ Admin::Engine.routes.draw do
   resources :matches do
     resource :match_conclusions, only: [:create]
   end
-  resources :people
+  resources :people do
+    resource :person_engagements, only: [:create]
+  end
   resource :session, only: [:new, :create, :destroy]
 end
