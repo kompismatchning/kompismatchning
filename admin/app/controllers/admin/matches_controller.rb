640d0747b91e67d069bdc2d99d76692cf186b50e
julia.ramstedt@varvet.com <Julia@Julias-MacBook-Air.local>
2016-10-15 14:24:00 +0200

    editable start date on matches

--- admin/app/controllers/admin/matches_controller.rb
+++ admin/app/controllers/admin/matches_controller.rb
@@ -7,7 +7,7 @@ module Admin
     private
 
     def resource_params
-      params.require(:match).permit(:established_id, :newcomer_id, :comment, :concluded_at, :status)
+      params.require(:match).permit(:established_id, :newcomer_id, :comment, :concluded_at, :started_at, :status)
     end
   end
 end
